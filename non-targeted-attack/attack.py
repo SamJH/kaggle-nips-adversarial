554d7742f687fd1902be44d8a91db7612ad2cb85
Karen Hambardzumyan <mahnerak@gmail.com>
2017-10-07 01:56:55 +0400

    Added non-targeted attack

---
+++ non-targeted-attack/attack.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import argparse
+import numpy as np
+from time import time
+from PIL import Image
+from tqdm import tqdm
+
+import keras
+from keras.models import Model, load_model
+from keras.layers import Input
+
+from lib import load_dataset
+from lib import PGD
+from lib import custom_object_scope
+
+z = time()
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir',   type=str,   required=True)
+    parser.add_argument('--output_dir',  type=str,   required=True)
+    parser.add_argument('--nb_classes',  type=int,   default=1000)
+    parser.add_argument('--width',       type=int,   default=299)
+    parser.add_argument('--height',      type=int,   default=299)
+    parser.add_argument('--nb_channels', type=int,   default=3)
+    parser.add_argument('--nb_iter',     type=int,   default=6)
+    parser.add_argument('--batch_size',  type=int,   default=4)
+    parser.add_argument('--max_eps',     type=float, default=16.0)
+    parser.add_argument('--eps_iter',    type=float, default=6.0)
+    parser.add_argument('--clip_min',    type=float, default=0.0)
+    parser.add_argument('--clip_max',    type=float, default=255.0)
+    parser.add_argument('--loss',        type=str,   default='minmax')
+    parser.add_argument('--nosign',      action='store_true')
+    args = parser.parse_args()
+    
+    args.eps_iter = args.max_eps // 3
+
+    paths, images = load_dataset(args.input_dir, limit=100)
+
+    with custom_object_scope():
+        voting_model = load_model('VOTING.model')
+        
+        
+    def build_attacker(white_box_model):
+        attack = PGD(eps=args.max_eps,
+                     eps_iter=args.eps_iter,
+                     clip_min=args.clip_min,
+                     clip_max=args.clip_max,
+                     from_logits=True,
+                     sign=not args.nosign,
+                     loss=args.loss)
+
+        x = Input(shape=(args.height, args.width, args.nb_channels))
+        y = white_box_model(x)
+
+        x0 = Input(shape=(args.height, args.width, args.nb_channels))
+        y0 = Input(shape=(args.nb_classes,))
+
+        adv_x = attack([x, y, x0, y0])
+
+        return Model(inputs=[x, x0, y0],
+                         outputs=adv_x,
+                         name='PGD_adv')
+
+    attacker = build_attacker(voting_model)
+
+    X = images
+    Y = voting_model.predict(X, batch_size=15, verbose=1)
+
+    def process_pred(input):
+        preds_max = np.max(input, axis=1, keepdims=True)
+        y = np.equal(input, preds_max).astype(np.float32)
+        y = y / np.sum(y, axis=1, keepdims=True)
+        return y
+
+    y0 = process_pred(Y)
+
+    
+    def craft(attacker, images):
+        start_X = X + np.random.uniform(-args.max_eps, +args.max_eps, size=X.shape)
+        start_X = np.clip(start_X, args.clip_min, args.clip_max)
+        adv_x = [ start_X  ] + [ None ] * args.nb_iter
+        
+        last_adv_X = start_X.copy()
+        for i in range(args.nb_iter):
+            for j in range(0, 100, 5):
+                last_adv_X[j:j+5] = attacker.predict([last_adv_X[j:j+5],
+                                                      X[j:j+5],
+                                                      Y[j:j+5]],
+                                                     batch_size=5,
+                                                     verbose=1)
+            
+                if time() > z + 500 - 30:
+                    return last_adv_X
+
+        return last_adv_X
+
+    adv_X = craft(attacker, X)
+    
+    for path, img in tqdm(zip(paths, adv_X)):
+        path = path.split('/') [-1]
+        path = os.path.join(args.output_dir, path)
+        Image.fromarray(img.astype(np.uint8)).save(path)
